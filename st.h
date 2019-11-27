ffbbe3dc1cd87f2ba44b16db8ce74db8ecb312db
Pavel Erofeev <ncuxomozg@gmail.com>
2019-11-27 15:01:48 +0300

    Common patches

--- st.h
+++ st.h
@@ -81,12 +81,15 @@ void die(const char *, ...);
 void redraw(void);
 void draw(void);
 
+void kscrolldown(const Arg *);
+void kscrollup(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
 void toggleprinter(const Arg *);
 
 int tattrset(int);
+int tisaltscr(void);
 void tnew(int, int);
 void tresize(int, int);
 void tsetdirtattr(int);
@@ -121,3 +124,5 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
+extern float alpha;
+extern float alphaUnfocussed;
